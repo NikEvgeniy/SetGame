d52151ce52d02658961d8a07b32b01f02def37a1
Evgeniy <Nik>
2022-02-21 22:17:15 +0300

    Initial Commit

---
+++ Set/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Set
+//
+//  Created by Evgeniy Nik on 21.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Evgeniy Nik on 21.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
