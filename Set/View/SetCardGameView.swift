fd31ad748d14d53bdca20ed8d35c37f161318b58
Evgeniy <Nik>
2022-04-03 14:06:10 +0200

    "Fly" animation for cards with Transition

--- Set/View/SetCardGameView.swift
+++ Set/View/SetCardGameView.swift
@@ -11,18 +11,40 @@ struct SetCardGameView: View {
     @StateObject var viewModel = SetCardGame()
     
     var body: some View{
-        Grid(viewModel.cards) { card in
-            CardView (card: card)
-                .onTapGesture {viewModel.choose(card: card)}
-                .padding(2)
+        GeometryReader { geometry in
+            Grid(viewModel.cards) { card in
+                CardView (card: card)
+                    .transition(AnyTransition.asymmetric(
+                        insertion: AnyTransition.offset(flyFrom(for: geometry.size)),
+                        removal: AnyTransition.offset(flyTo(for:geometry.size))
+                            .combined(with: AnyTransition.scale(scale: 0.5))))
+                    .onTapGesture {
+                        withAnimation(Animation.easeInOut(duration: 1.00)){
+                        viewModel.choose(card: card)
+                        }
+                    }.padding(2)
+            } .onAppear{
+                withAnimation(Animation.easeInOut(duration: 1.0)){
+                    viewModel.deal()
+                }
+            }
+            .padding(2)
+            .background(tableColor.edgesIgnoringSafeArea(.all) )
         }
-        .onAppear{viewModel.deal()}
-        .padding(2)
-        .background(tableColor.edgesIgnoringSafeArea(.all) )
     }
     // MARK: - Drawing Constants
     private var tableColor: Color{
         Color(UIColor(red: 0, green: 0.5, blue: 0, alpha: 1))}
+    
+    private func flyFrom(for size: CGSize) -> CGSize {
+        CGSize(width: 0.0 /*CGFloat.random(in: -size.width/2...size.width/2)*/,
+               height: size.height)
+    }
+    
+    private func flyTo(for size:CGSize) -> CGSize {
+        CGSize(width: CGFloat.random(in: -3*size.width...3*size.width),
+                   height: CGFloat.random(in: -2*size.height...(-size.height)))
+    }
 }
 
 struct CardView: View {
@@ -35,8 +57,8 @@ struct CardView: View {
                 .background(Color.white)
                 .cornerRadius(cornerRadius)
                 .overlay(
-                RoundedRectangle(cornerRadius: cornerRadius)
-                    .stroke(highlightColor(), lineWidth: borderLineWidth))
+                    RoundedRectangle(cornerRadius: cornerRadius)
+                        .stroke(highlightColor(), lineWidth: borderLineWidth))
         }
     }
     // MARK: - Drawing Constants & Helper Functions
@@ -53,7 +75,7 @@ struct CardView: View {
                 color = colorsBorder [1]
             }else{
                 color = colorsBorder [2]
-
+                
             }
         }
         return color
